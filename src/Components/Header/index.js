6a69f688cdf27ce0b7eed5e4351303278dbc8d52
Erik Davis <38073835+eriklamontdavis@users.noreply.github.com>
2019-12-28 13:40:12 +0100

    add copy to clipboard button
    Need to add:
    npm install --save react react-copy-to-clipboard

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -4,31 +4,44 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 
 import { Consumer } from "../Context";
 
-const Header = ({ min, deadline }) => {
-  return (
-    <Consumer>
-      {({ data }) => (
-        <React.Fragment>
-          <div className="header">
-            <h1>{data.min-data.guests.length}</h1>
-            <p>
-              more people needed in <span>{data.deadline}</span> for this
-              gathering to happen.
-            </p>
+class Header extends React.Component {
+  state = {
+    value: window.location.href,
+    copied: false
+  };
 
-            {/*
-            <CopyToClipboard onCopy={this.onCopy}>
-              <button className="copyLink">
-                <i className="ticketIcon" />
-                Copy invite link
-              </button>
-            </CopyToClipboard>
-            */}
-          </div>
-        </React.Fragment>
-      )}
-    </Consumer>
-  );
-};
+  render() {
+    const { deadline, guests, min } = this.props;
+
+    return (
+      <Consumer>
+        {({ data }) => (
+          <React.Fragment>
+            <div className="header">
+              <h1>{data.min - data.guests.length}</h1>
+              <p>
+                more people needed in <span>{data.deadline}</span> for this
+                gathering to happen.
+              </p>
+
+              <CopyToClipboard
+                text={this.state.value}
+                onCopy={() => this.setState({ copied: true })}
+              >
+                <button>
+                  {this.state.copied ? (
+                    <span style={{ color: "green" }}>Copied.</span>
+                  ) : (
+                    "Tap to copy link"
+                  )}
+                </button>
+              </CopyToClipboard>
+            </div>
+          </React.Fragment>
+        )}
+      </Consumer>
+    );
+  }
+}
 
 export default Header;
